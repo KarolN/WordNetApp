c529473ae96dad8f8b04988d7132f805ad2bc847
Alicjaaa <alicja.majewska94@o2.pl>
2017-04-03 13:51:38 +0200

    removing punctuation marks and counting word frequency appearance in text

--- app/services/wordService.js
+++ app/services/wordService.js
@@ -10,29 +10,41 @@ angular.module("wordApp").factory("wordService", ["$http", "$q", "$timeout", "ws
                 .then(function (response) {
                     if (response.data.status !== "DONE") {
                         $timeout(function () {
-                            return checkTaskProgress(id).then(function (data) { resolve(data); });
+                            return checkTaskProgress(id).then(function (data) {
+                                resolve(data);
+                            });
                         }, 60);
                     } else {
                         resolve(response.data);
                     }
                 })
         });
-    };
+    }
+
+    function removePunctuationMarks(textToAnalize) {
+        var replacedText = textToAnalize.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]|\\|[0-9]/g, "");
+        console.log("removePunctuationMarks: " + replacedText);
+        return replacedText;
+    }
 
     function getWordData(textToAnalize) {
-        dataService.sendDataForProcessing({ "lpmn": "any2txt|wcrft2({\"morfeusz2\":false})|wsd", "text": textToAnalize })
+        var textToAnalizeNoMarks = removePunctuationMarks(textToAnalize);
+        dataService.sendDataForProcessing({
+            "lpmn": "any2txt|wcrft2({\"morfeusz2\":false})|wsd",
+            "text": textToAnalizeNoMarks
+        })
             .then(function (response) {
                 return checkTaskProgress(response.data)
             }).then(function (data) {
-                dataService.getProcessedData(data.value[0].fileID).then(function (response) {
-                    return wsdXmlParsingService.parseWsdXml(response.data).then(function (data) {
-                        dataService.getSynsets(synsetService.parseSynsetsIds(data)).then(function(response){
-                            console.log(response.data);
-                        });
+            dataService.getProcessedData(data.value[0].fileID).then(function (response) {
+                return wsdXmlParsingService.parseWsdXml(response.data).then(function (data) {
+                    dataService.getSynsets(synsetService.parseSynsetsIds(data)).then(function (response) {
+                        console.log(response.data);
                     });
                 });
             });
-    };
+        });
+    }
 
     return {
         getWordData: getWordData
--- app/services/wsdXmlParsingService.js
+++ app/services/wsdXmlParsingService.js
@@ -1,47 +1,64 @@
+angular.module("wordApp").factory("wsdXmlParsingService", ["$q", function ($q) {
 
-
-angular.module("wordApp").factory("wsdXmlParsingService", ["$q", function($q){
-
-    function parseXml(xml){
+    function parseXml(xml) {
         var x2js = new X2JS();
-         var jsonFromXml = x2js.xml_str2json(xml);
-         return jsonFromXml;
+        var jsonFromXml = x2js.xml_str2json(xml);
+        return jsonFromXml;
     }
 
-    function parseSynsRank(text){
+    function parseSynsRank(text) {
         var synsets = [];
 
         var idAndProbs = text.indexOf(" ") !== -1 ? text.split(" ") : [text];
-        
-        _.each(idAndProbs, function(item){
+
+        _.each(idAndProbs, function (item) {
             var idAndProb = item.split("/");
-            var synset = {id: idAndProb[0], probability :idAndProb[1]};
-            synsets.push(synset);          
+            var synset = {id: idAndProb[0], probability: idAndProb[1]};
+            synsets.push(synset);
         });
         return synsets;
     }
 
-    function getWordsWithData(parsedObject){
+    function createNewWord(tok) {
+        var word = {
+            name: tok.lex.base,
+            count: 1
+        };
+        _.each(tok.prop, function (prop) {
+            if (prop._key === "sense:ukb:syns_rank") {
+                word.synsets = parseSynsRank(prop.__text);
+            }
+        });
+        return word;
+    }
+
+    function incrementWordCount(foundWord) {
+        foundWord.count += 1;
+    }
+
+    function getWordsWithData(parsedObject) {
         var result = [];
-        _.each(parsedObject.chunkList.chunk, function(chunk){
-            _.each(chunk.sentence.tok, function(tok){
-                var word = {
-                    name: tok.lex.base,
-                    synsets: []
-                };
-                result.push(word);
-                _.each(tok.prop, function(prop){
-                    if(prop._key === "sense:ukb:syns_rank"){
-                       word.synsets = parseSynsRank(prop.__text);
-                    }
+        console.log(parsedObject);
+        _.each(parsedObject.chunkList, function (chunk) {
+            _.each(chunk.sentence.tok, function (tok) {
+                var foundWord = _.find(result, function (resultWord) {
+                    return resultWord.name === tok.lex.base;
                 });
+                if (foundWord) {
+                    incrementWordCount(foundWord);
+                } else {
+                    var word = createNewWord(tok);
+                    result.push(word);
+                }
             });
         });
+        console.log("getWordsWithData result: ");
+        console.log(result);
         return result;
     }
 
-    function parseWsdXml(input){
-        return $q(function(resolve, reject){
+    function parseWsdXml(input) {
+        return $q(function (resolve, reject) {
             var parsedJson = parseXml(input);
             var result = getWordsWithData(parsedJson);
             resolve(result);
